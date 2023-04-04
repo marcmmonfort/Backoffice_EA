4720f7b1a242b6627ac9f2165644b0791b280d5e
oscarboullosa <oscar.boullosa@estudiantat.upc.edu>
2023-04-04 19:20:06 +0200

    Añadida navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LogInComponent } from './components/log-in/log-in.component';
+//import { LogInComponent } from './components/log-in/log-in.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './home/app.component';
+
+const routes: Routes = [
+  //{ path: '', component: AppComponent },
+  { path: 'listUsers', component: ListUserComponent }
+];
 
 @NgModule({
   declarations: [
+    ListUserComponent,
     AppComponent,
-    LogInComponent
+    //NavigationComponent,
+    //ListUserComponent,
+    //LogInComponent
   ],
+  
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule,
     FormsModule, //La he añadido por el tutorial
     ReactiveFormsModule //La he añadido por el tutorial
@@ -20,4 +33,5 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
